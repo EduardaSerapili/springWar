b51b6e096aa62cc36ad4aa0b52b9019096cfe0d8
EduardaSerapili <eduardaserapili@gmail.com>
2024-06-04 21:21:14 -0300

    first commit

---
+++ src/main/java/com/example/springwar/SpringwarApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwar/SpringwarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
